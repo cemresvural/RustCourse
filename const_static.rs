9e9146bf0ec7562ff63caae29bff10dc025f6ff6
CEMRE SEVAL VURAL <50405470+cemresvural@users.noreply.github.com>
2022-04-18 06:26:20 +0300

    Add files via upload

---
+++ const_static.rs
@@ -0,0 +1,22 @@
+#[allow(dead_code)]
+#[allow(unused_variables)]
+use std::mem;
+const VERI:i8=23;//belirli bir adresi yok
+static  mut m:i8=22;//mutable olabilir unsafe ile
+
+fn main() {
+let a=44;
+    {
+        let a=56;
+        println!("dongunun içi a:{}",a);
+
+    }
+println!(" dongunun disi a:{}",a);
+    println!("veri:{}",VERI);
+
+    unsafe {
+        m=66;
+    }
+    println!("m:{}",m);
+
+}
